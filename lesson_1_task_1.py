322a54f8d92d10abf36bd77a8b047e49ed7a46f3
Artur Grinchuk <a.grinchuk@yahoo.com>
2024-08-26 14:42:24 +0700

    Initial commit

---
+++ lesson_1_task_1.py
@@ -0,0 +1,3 @@
+my_name = "Artur"
+
+print(my_name)
---
+++ lesson_1_task_7.py
@@ -0,0 +1,51 @@
+def out1():
+    print(1, end="")
+
+
+def out2():
+    print(2, end="")
+
+
+def out3():
+    print(3, end="")
+
+
+def out4():
+    print(4, end="")
+
+
+def out5():
+    print(5, end="")
+
+
+def out6():
+    print(6, end="")
+
+
+def out7():
+    print(7, end="")
+
+
+def out8():
+    print(8, end="")
+
+
+def out9():
+    print(9, end="")
+
+
+def out0():
+    print(0, end="")
+
+
+out8()
+out8()
+out0()
+out0()
+out5()
+out5()
+out5()
+out3()
+out5()
+out3()
+out5()
